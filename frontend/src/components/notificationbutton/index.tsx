f96bb07e9a01a1cfb821ab0256cdee2f7e899c10
William Pagani <williamlg_pagani@hotmail.com>
2022-11-20 10:33:40 -0300

    Send notification

--- frontend/src/components/notificationbutton/index.tsx
+++ frontend/src/components/notificationbutton/index.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios'
 import icon from '../../assets/img/notificationicon.svg'
+import { BASE_URL } from '../../utils/request'
 import './style.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: Number
+}
+
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+         .then(response =>{
+            console.log("Sucesso")
+         })
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar" />
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />            
         </div>
     )
 }
